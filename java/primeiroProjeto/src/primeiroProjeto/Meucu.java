6e9590b83bfaf3ca5704832b35b6ceba480a6c2a
Emii <emily.g.dias@aluno.senai.br>
2025-09-12 16:00:41 -0300

    Add files via upload

---
+++ java/primeiroProjeto/src/primeiroProjeto/Meucu.java
@@ -0,0 +1,15 @@
+package primeiroProjeto;
+
+public class Meucu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nnnnk = 1;
+		 while(nnnnk <= 5) {
+			 System.out.println(nnnnk);
+			 nnnnk++;
+		 }
+	}
+
+}
---
+++ java/primeiroProjeto/src/primeiroProjeto/While2.java
@@ -0,0 +1,54 @@
+package primeiroProjeto;
+
+import java.util.Scanner;
+
+public class While2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int contador = 10;
+		while(contador <= 30) {
+			System.out.println(contador);
+			contador++;
+		}
+		
+		int num1 = 1;
+		int num2 = 0;
+		while (num1 <= 3) {
+		System.out.println("diga um numero");
+		int total = scanner.nextInt();
+		num2 = total + num2;
+		num1++;
+		}System.out.println("o total é " + num2 );
+		
+		
+		
+		int num4 = 1;
+		int num5 = 1;
+		while(num4 <= 5) {
+			System.out.println("diga um numero ");
+			int odeioessamerda = scanner.nextInt();
+			num5= odeioessamerda * num5;
+			num4++;	
+		}System.out.println("o resultado é " + num5);
+		
+		
+		 int nao = 1;
+		 System.out.println("fala um numero ");
+		 int sla = scanner.nextInt();
+		 while(sla >= nao) {
+			 System.out.println(nao);
+			 nao++;
+		 }
+		 
+		 int nnnnk = 1;
+		 while(nnnnk <= 5) {
+			 System.out.println(nnnnk);
+			 nnnnk++;
+		 }
+		
+	}
+
+}
